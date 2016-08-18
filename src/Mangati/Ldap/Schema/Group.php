b065e3d8ff545e0bac150696bbf7984040df9dc7
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2016-08-18 13:33:46 -0300

    new schema group

---
+++ src/Mangati/Ldap/Schema/Group.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Mangati\Ldap\Schema;
+
+/**
+ * GroupOfNames
+ *
+ * @author Rogério Lino <rogeriolino@gmail.com>
+ */
+class GroupOfNames extends Top
+{
+    
+    public function objectClasses()
+    {
+        $base = parent::objectClasses();
+        $base[] = 'groupOfNames';
+        return $base;
+    }
+    
+    public function rdnAttributeName()
+    {
+        return 'cn';
+    }
+
+    public function requiredAttributes()
+    {
+        $base = parent::requiredAttributes();
+        return array_merge($base, [
+            'cn',
+            'member'
+        ]);
+    }
+
+    public function availableAttributes()
+    {
+        $base = parent::availableAttributes();
+        return array_merge($base, [
+            'cn' => [
+                'class' => \Mangati\Ldap\Attribute\TextAttribute::class,
+                'single_valued' => true
+            ],
+            'member' => [
+                'class' => \Mangati\Ldap\Attribute\TextAttribute::class,
+                'single_valued' => false
+            ],
+            'description' => [
+                'class' => \Mangati\Ldap\Attribute\TextAttribute::class,
+                'single_valued' => false
+            ]
+        ]);
+    }
+}
